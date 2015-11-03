d8713185c5905a7d09b65903e0a44c251bac6d89
popcornmix <popcornmix@gmail.com>
2015-11-03 00:55:18 +0000

    Kick out some of the poorer shaders and add some new ones

--- src/main.cpp
+++ src/main.cpp
@@ -61,10 +61,8 @@ const std::vector<Preset> g_presets =
    {"LED spectrum by simesgreen",               "ledspectrum.frag.glsl",            99, -1, -1, -1},
    {"2D LED Spectrum by uNiversal",             "2Dspectrum.frag.glsl",             99, -1, -1, -1},
    {"Audio Eclipse by airtight",                "audioeclipse.frag.glsl",           99, -1, -1, -1},
-   {"Audio Reaktive by choard1895",             "audioreaktive.frag.glsl",          99, -1, -1, -1},
    {"AudioVisual by Passion",                   "audiovisual.frag.glsl",            99, -1, -1, -1},
    {"Beating Circles by Phoenix72",             "beatingcircles.frag.glsl",         99, -1, -1, -1},
-   {"BPM by iq",                                "bpm.frag.glsl",                    99, -1, -1, -1},
    {"Dancing Metalights by Danguafare",         "dancingmetalights.frag.glsl",      99, -1, -1, -1},
    {"The Disco Tunnel by poljere",              "discotunnel.frag.glsl",             2, 13, 99, -1},
    {"Gameboy by iq",                            "gameboy.frag.glsl",                99, -1, -1, -1},
@@ -72,12 +70,21 @@ const std::vector<Preset> g_presets =
    {"Simplicity Galaxy by CBS",                 "simplicitygalaxy.frag.glsl",       99, -1, -1, -1},
    {"Sound Flower by iq",                       "soundflower.frag.glsl",            99, -1, -1, -1},
    {"Sound sinus wave by Eitraz",               "soundsinuswave.frag.glsl",         99, -1, -1, -1},
-   {"Spectrometer by jaba",                     "spectrometer.frag.glsl",           99, -1, -1, -1},
    {"symmetrical sound visualiser by thelinked","symmetricalsound.frag.glsl",       99, -1, -1, -1},
    {"Twisted Rings by poljere",                 "twistedrings.frag.glsl",           99, -1, -1, -1},
    {"Undulant Spectre by mafik",                "undulantspectre.frag.glsl",        99, -1, -1, -1},
    {"Waves Remix by ADOB",                      "wavesremix.frag.glsl",             99, -1, -1, -1},
-   {"Circle Wave by TekF",                      "circlewave.frag.glsl",             99, -1, -1, -1}};
+   {"Circle Wave by TekF",                      "circlewave.frag.glsl",             99, -1, -1, -1},
+   {"Metaballs by the23",                       "metaballs.frag.glsl",              99, -1, -1, -1},
+   {"Explosive by epal",                        "explosive.frag.glsl",              99, -1, -1, -1},
+   {"Circular by Mx7f",                         "circular.frag.glsl",               99, -1, -1, -1},
+   {"AudioSurf by 4rknova",                     "audiosurf.frag.glsl",              99, -1, -1, -1},
+   {"AudioSurf3 by 4rknova",                    "audiosurf3.frag.glsl",             99, -1, -1, -1},
+   {"Flag by 4rknova",                          "flag.frag.glsl",                   99, -1, -1, -1},
+   {"Audio Fl0wer by daek",                     "audioflower.frag.glsl",            99, -1, -1, -1},
+   {"Sine Waves by popcornmix",                 "sinewaves.frag.glsl",              99, -1, -1, -1},
+   {"Alhambra on LSD by mfagerlund",            "alhambra.frag.glsl",               99, -1, -1, -1},
+   {"Colorspace by bloxard",                    "colorspace.frag.glsl",             99, -1, -1, -1}};
 #else
 const std::vector<Preset> g_presets =
   {
